e8ab70d73b355dff0ca41869dc415d8d28848e14
Rizadh Nizam <rizadh@icloud.com>
2016-08-28 21:10:02 -0400

    Add basic skeleton client code

--- client.c
+++ client.c
@@ -1,7 +1,27 @@
 struct user {
     int id;
     char handle[MAX_HANDLE];
+} *userlist;
+
+void parseargs(int argc, char **argv);
+void connect_server();
+void handle_input();
+
+int server_fd;
+int server_port = DEFAULT_PORT;
+
+int main(int argc, char **argv) {
+    parseargs(argc, argv);
+    connect_server();
+    while (1)
+        handle_input();
 }
 
-int main() {
+void parseargs(int argc, char **argv) {
+}
+
+void connect_server() {
+};
+
+void handle_input() {
 }
