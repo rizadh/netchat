12e7aa7a2a4e70c0abc04c6258bcebd3b6b171e7
Rizadh Nizam <rizadh@icloud.com>
2016-08-29 01:48:32 -0400

    Reorganize User and Server fields into typdef-ed structs
    Also add next entry to struct user

--- client.c
+++ client.c
@@ -1,14 +1,30 @@
-struct user {
+#include <stdio.h>
+
+typedef struct user {
     int id;
     char handle[MAX_HANDLE];
-} *userlist;
+    struct user *next;
+} User;
+
+typedef struct server {
+    int fd;
+    int port;
+    char *address;
+    char buf[MAX_LEN];
+    int num_bytes;
+} Server;
 
 void parseargs(int argc, char **argv);
 void connect_server();
 void handle_input();
 
-int server_fd;
-int server_port = DEFAULT_PORT;
+Server server = {
+    .port = DEFAULT_PORT,
+    .address = NULL,
+    .num_bytes = 0
+};
+
+User *userlist = NULL;
 
 int main(int argc, char **argv) {
     parseargs(argc, argv);
